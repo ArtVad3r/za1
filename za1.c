4411c870dede828868b627018a13c3d649329fa8
ArtVad3r <39739339+ArtVad3r@users.noreply.github.com>
2018-05-29 19:33:11 +0200

    Update za1.c

--- za1.c
+++ za1.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-#define max 100
 int main()
 {
-	int cisla[max], i = 0, suma = 0;
+    int i, suma = 0;
 
-	do
-	{
-		scanf("%d", &cisla[i]);
-		suma += cisla[i];
-	} while (cisla[i++] != 0);
+    do
+    {
+        scanf("%d", &i);
+        suma += i;
+    } while (i != 0);
 
-	printf("\nsuma: %d \n", suma);
-	return 0;
+    printf("\nsuma: %d \n", suma);
+    return 0;
 }
