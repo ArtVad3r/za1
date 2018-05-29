710eed54f051649d4af54035780435b5ffab6511
ArtVad3r <39739339+ArtVad3r@users.noreply.github.com>
2018-05-29 20:03:59 +0200

    Update zc2.c

--- zc2.c
+++ zc2.c
@@ -1,87 +1,81 @@
 #include <stdio.h>
-#include <stdlib.h>
 #define MAX 20
 
-void NacitajMaticu(int r, int s, int A[MAX][MAX])
-{
-	int i, j;
-
-	for (i = 0; i<r; i++)
-	{
-		for (j = 0; j<s; j++)
-			scanf("%d", &A[i][j]);
-	}
-}
-
-void VypisMaticu(int r, int s, int A[MAX][MAX])
-{
-	int i, j;
-
-	for (i = 0; i<r; i++)
-	{
-		for (j = 0; j<s; j++)
-			printf("%5d", A[i][j]);
-		printf("\n");
-	}
-}
-
-void NeparnePrvkyPodDiagonalou(int r, int s, int A[MAX][MAX],
-	int neparne_prvky_pod_diag_f[MAX], int *p_pocet_neparn_prvkov_f, double *p_priemer_f, int *p_suma_f)
-{
-	int i, j, mod, k = 0, pocet_neparnych = 0, suma = 0, l = 0;
-	double priemer = 0;
-
-	for (i = 0; i<r; i++)
-	{
-		for (j = 0; j<s; j++)
-		{
-			mod = A[i][j] % 2;
-			if ((mod != 0) && (i>j))
-			{
-				neparne_prvky_pod_diag_f[k] = A[i][j];
-				pocet_neparnych++;
-				suma += neparne_prvky_pod_diag_f[k];
-				k++;
-			}
-		}
-	}
-	priemer = suma / (double)pocet_neparnych;
-
-
-	*p_pocet_neparn_prvkov_f = pocet_neparnych;
-	*p_priemer_f = priemer;                    ///premenna do ktorej si ulozim adresu inej premennej
-	*p_suma_f = suma;            		 ///void mi vrati len prvky a ja chcem celu hodnotu cize preto som pouzil pointre
-}
-int main()
-{
-	int i, riadok, stlpec, pocet_neparn_prvkov, suma;
-	int A[MAX][MAX], neparne_prvky_pod_diag[MAX];
-	double priemer;
-
-	int *p_pocet_neparn_prvkov, *p_suma;
-	double *p_priemer;
-
-	p_pocet_neparn_prvkov = &pocet_neparn_prvkov;
-	p_priemer = &priemer;
-	p_suma = &suma;
-
-	scanf("%d %d", &riadok, &stlpec);
-
-	NacitajMaticu(riadok, stlpec, A);
-	printf("Matica A:\n");
-	VypisMaticu(riadok, stlpec, A);
-
-	NeparnePrvkyPodDiagonalou(riadok, stlpec, A, neparne_prvky_pod_diag, p_pocet_neparn_prvkov, p_priemer, p_suma);
-
-	printf("\nNeparne prvky pod hlavnou diagonalou matice A su:  ");
-	for (i = 0; i<pocet_neparn_prvkov; i++)
-		printf("%3d", neparne_prvky_pod_diag[i]);
-
-	printf("\nPocet neparnych prvkov pod hlavnou diagonalou matice A je: %d", pocet_neparn_prvkov);
-	printf("\nPriemer neparnych prvkov pod hlavnou diagonalou matice A je: %.2f", priemer);
-	printf("\nSuma neparnych prvkov pod hlavnou diagonalou matice A je: %d", suma);
-	printf("\n");
-
-	return 0;
+void NacitajMaticu(int r, int s, int A[MAX][MAX]){
+    int i, j;
+    printf("Zadajte prvky matice %d x %d:\n", r, s);
+    for(i=0; i<r; i++){
+        for(j=0;j<s;j++)
+            scanf("%d", &A[i][j]);
+    }
 
 }
+
+void VypisMaticu(int r, int s, int A[MAX][MAX]){
+    int i, j;
+    printf("Vypis matice %d x %d:\n", r, s);
+    for(i=0; i<r; i++){
+        for(j=0;j<s;j++)
+            printf("%5d", A[i][j]);
+        printf("\n");
+    }
+
+
+}
+
+void NeparnePrvkyPodDiagonalou(int r, int s, int A[MAX][MAX], int neparne_prvky_pod_diag_f[MAX], int *p_pocet_f, int *p_suma_f, double *p_priemer_f){
+    //zavedenie premennych na docasne pouzitie
+    int i, j, k=0, mod=0;
+
+    //cyklus, prechadzanie matice a nasledne zapisovanie udajov pri vyhovujucej pozicii v matici
+    for(i=0; i<r; i++){
+        for(j=0;j<s;j++) {
+            mod = A[i][j] % 2;
+            if (i > j && mod != 0) {
+                neparne_prvky_pod_diag_f[k++] = A[i][j];
+                *p_pocet_f += 1;
+                *p_suma_f += A[i][j];
+            }
+        }
+    }
+    *p_priemer_f = *p_suma_f / (double)*p_pocet_f;
+
+}
+
+int main(int argc, char* argv[]) {
+    //definovanie premennych a pointerov
+    int r_a, s_a, A[MAX][MAX], neparne_prvky_pod_diag_f[MAX], *p_suma_f, suma_neparnych=0, pocet_neparnych=0, *p_pocet_f;
+    double priemer_neparnych=0, *p_priemer_f;
+
+    //zavedenie pointerov
+    p_pocet_f=&pocet_neparnych;
+    p_suma_f=&suma_neparnych;
+    p_priemer_f=&priemer_neparnych;
+
+
+    //zadanie rozmerov matice s kontrolou neplatnej velkosti
+    do {
+        printf("Zadajte rozmery matice (max 19x19):\n");
+        scanf("%d %d", &r_a, &s_a);
+
+        if (r_a>19 || s_a>19)
+            printf("\n[CHYBA] Zadali ste vacsi rozmer, ako je povoleny. Zadajte rozmery znovu: \n\n");
+        else if(r_a<1, s_a<1)
+            printf("\n[CHYBA] Zadali ste nezmyselnu velkost matice. Zadajte rozmery znovu: \n \n");
+
+    }while(r_a>19 || s_a>19 || r_a <1, s_a <1);
+
+
+    //nacitanie a vypisanie matice
+    NacitajMaticu(r_a, s_a, A);
+    VypisMaticu(r_a, s_a, A);
+
+    //spustenie algoritmu na analyzu a spracovanie matice, posielanie adries pointerov
+    NeparnePrvkyPodDiagonalou(r_a , s_a, A, neparne_prvky_pod_diag_f, p_pocet_f, p_suma_f, p_priemer_f);
+
+    //vypisanie vysledkov
+    printf("Priemer neparnych cisel trojuholniku pod diagonalou je: %.2f\nSuma neparnych cisel trojuholniku pod diagonalou je: %d\nPocet neparnych cisel pod diagonalou je: %d\n", priemer_neparnych, suma_neparnych, pocet_neparnych);
+
+
+
+    return 0;
