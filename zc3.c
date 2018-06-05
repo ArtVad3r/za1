4b6698afd6abbec13e72d80d8a26286dbd634f70
ArtVad3r <39739339+ArtVad3r@users.noreply.github.com>
2018-06-05 18:03:44 +0200

    Update zc3.c

--- zc3.c
+++ zc3.c
@@ -4,131 +4,129 @@
 
 typedef struct CPLX
 {
-	double real;
-	double imaginary;
+    double realne;
+    double imaginarne;
 }CPLX;
 
 struct CPLX add(struct CPLX a, struct CPLX b)
 {
-	CPLX ret;
-	ret.real = a.real + b.real;
-	ret.imaginary = a.imaginary + b.imaginary;
-	return ret;
+    CPLX cfg;
+    cfg.realne = a.realne + b.realne;
+    cfg.imaginarne = a.imaginarne + b.imaginarne;
+    return cfg;
 }
 
 struct CPLX sub(struct CPLX a, struct CPLX b)
 {
-	CPLX ret;
-	ret.real = a.real - b.real;
-	ret.imaginary = a.imaginary - b.imaginary;
-	return ret;
+    CPLX cfg;
+    cfg.realne = a.realne - b.realne;
+    cfg.imaginarne = a.imaginarne - b.imaginarne;
+    return cfg;
 }
 struct CPLX mult(struct CPLX a, struct CPLX b)
 {
-	CPLX ret;
-	ret.real = a.real * b.real - a.imaginary * b.imaginary;
-	ret.imaginary = a.real * b.imaginary + a.imaginary * b.real;
-	return ret;
+    CPLX cfg;
+    cfg.realne = a.realne * b.realne - a.imaginarne * b.imaginarne;
+    cfg.imaginarne = a.realne * b.imaginarne + a.imaginarne * b.realne;
+    return cfg;
 }
 
 int main(int argc, char* argv[])
 {
-	double real1; 
-	double real2;
-	double real3;
-	double imaginary1, imaginary2, imaginary3;
-	char operant, end, end2, end3;
-	FILE *readF, *writeF;
-	readF = fopen("vstup_2_3_komplex_cisla_operatory.txt", "r");
-	writeF = fopen("vystup.txt", "w");
+    double realne1;
+    double realne2;
+    double realne3;
+    double imaginarne1, imaginarne2, imaginarne3;
+    char operant, end, end2, end3;
+    FILE *readF, *writeF;
+    readF = fopen("vstup_2_3_komplex_cisla_operatory.txt", "r");
+    writeF = fopen("vystup.txt", "w");
 
-	if (readF == NULL)
-	{
+    if (readF == NULL)
+    {
         printf ("File not found\n");
-		printf("Press 'Enter' to exit\n");
-		getchar();
+        printf("Press 'Enter' to exit\n");
+        getchar();
         return 1;
     }
 
 
-	while (fscanf(readF, "%lf %*c %*c %lf %c %lf %*c %*c %lf", &real1, &imaginary1, &operant, &real2, &imaginary2) != EOF)
-	{
-		CPLX first, second, third, final;
-		first.real = real1;
-		first.imaginary = imaginary1;
-		second.real = real2;
-		second.imaginary = imaginary2;
+    while (fscanf(readF, "%lf %*c %*c %lf %c %lf %*c %*c %lf", &realne1, &imaginarne1, &operant, &realne2, &imaginarne2) != EOF)
+    {
+        CPLX first, second, third, final;
+        first.realne = realne1;
+        first.imaginarne = imaginarne1;
+        second.realne = realne2;
+        second.imaginarne = imaginarne2;
 
-		switch (operant)
-		{
-			case '+':
-			{
-				final = add(first, second);
-				break;
-			}
-			case '-':
-			{
-				final = sub(first, second);
-				break;
-			}
-			case '*':
-			{
-				final = mult(first, second);
-				break;
-			}
-			default:
-			{
-				break;
-			}
-		}
+        switch (operant)
+        {
+            case '+':
+            {
+                final = add(first, second);
+                break;
+            }
+            case '-':
+            {
+                final = sub(first, second);
+                break;
+            }
+            case '*':
+            {
+                final = mult(first, second);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
 
-		end = getc(readF);
-		if (end == '\n' || end == EOF)
-		{
-			fprintf(writeF,"(%lf + i %lf) %c (%lf + i %lf) = %lf + i %lf\n", first.real, first.imaginary, operant, second.real, second.imaginary, final.real, final.imaginary);	
-		}
-		else 
-		{
-			fscanf(readF, "%*c %*c %lf %*c %*c %lf", &real3, &imaginary3);
-			third.real = real3;
-			third.imaginary = imaginary3;
+        end = getc(readF);
+        if (end == '\n' || end == EOF)
+        {
+            fprintf(writeF,"(%lf + i %lf) %c (%lf + i %lf) = %lf + i %lf\n", first.realne, first.imaginarne, operant, second.realne, second.imaginarne, final.realne, final.imaginarne);
+        }
+        else
+        {
+            fscanf(readF, "%*c %*c %lf %*c %*c %lf", &realne3, &imaginarne3);
+            third.realne = realne3;
+            third.imaginarne = imaginarne3;
 
-			switch (operant)
-			{
-				case '+':
-				{
-					final = add(final, third);
-					break;
-				}
-				case '-':
-				{
-					final = sub(final, third);
-					break;
-				}
-				case '*':
-				{
-					final = mult(final, third);
-					break;
-				}
-				default:
-				{
-					break;
-				}
-			}
+            switch (operant)
+            {
+                case '+':
+                {
+                    final = add(final, third);
+                    break;
+                }
+                case '-':
+                {
+                    final = sub(final, third);
+                    break;
+                }
+                case '*':
+                {
+                    final = mult(final, third);
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
 
-			fprintf(writeF,"(%lf + i %lf) %c (%lf + i %lf) %c (%lf + i %lf) = %lf + i %lf\n", first.real, first.imaginary, operant, second.real, second.imaginary, operant, third.real, third.imaginary, final.real, final.imaginary);	
+            fprintf(writeF,"(%lf + i %lf) %c (%lf + i %lf) %c (%lf + i %lf) = %lf + i %lf\n", first.realne, first.imaginarne, operant, second.realne, second.imaginarne, operant, third.realne, third.imaginarne, final.realne, final.imaginarne);
+        }
+        printf("%lf + i %lf\n",final.realne, final.imaginarne);
 
-		}
-		printf("%lf + i %lf\n",final.real, final.imaginary);
-		
 
-	}
+    }
 
-	fclose(readF);
-	fclose(writeF);
+    fclose(readF);
+    fclose(writeF);
 
-	printf("Press 'Enter' to exit\n");
-	getchar();
-	return 0;
+    printf("Press 'Enter' to exit\n");
+    getchar();
+    return 0;
 }
-
